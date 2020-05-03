698bba80fa0f7b7633f58af57ce9088bf8e18491
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-03 14:29:57 +0530

    Add files via upload

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import yaml
+import spamwatch
 from telethon import TelegramClient
 import telegram.ext as tg
 
@@ -114,6 +115,14 @@ KITE =  CONFIG['kite']
 SUDO_USERS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 SUDO_USERS.add(802002142)
+# SpamWatch
+spamwatch_api = CONFIG['sw_api']
+
+if spamwatch_api == "None":
+    sw = None
+    LOGGER.warning("SpamWatch API key is missing! Check your config.env.")
+else:
+    sw = spamwatch.Client(spamwatch_api)
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
