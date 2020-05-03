702e2137d2b2acac9b91b35de1a5028285638f22
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-03 14:44:14 +0530

    Update __init__.py

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -96,10 +96,6 @@ STRICT_GMUTE = CONFIG['strict_gmute']
 WEBHOOK = CONFIG['webhook']
 URL = CONFIG['url']
 BAN_STICKER = CONFIG['ban_sticker']
-spamwatch_api = CONFIG['sw_api']
-
-sw = spamwatch.Client(spamwatch_api)
-
 
 # zoldyck family
 ALLUKA = CONFIG['alluka']
@@ -122,7 +118,6 @@ SUDO_USERS.add(802002142)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
-
 dispatcher = updater.dispatcher
 tbot = TelegramClient("alluka", API_KEY, API_HASH)
 
